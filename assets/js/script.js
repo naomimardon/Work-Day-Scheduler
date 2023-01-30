c34b9901fcf7618c98624eeed7f7087e379b8975
Naomi Mardon <naomimardon@Naomis-MBP.broadband>
2023-01-30 14:07:29 +0000

    dynamically added first timeblock using jquery

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,7 @@ $(function(){
 
 //DOM Variables
 let currentDay = $("#currentDay");
-
+let container = $(".container");
 
 //function to set the day in the jumbotron
 function displayDay() {
@@ -10,41 +10,38 @@ function displayDay() {
     currentDay.text(day);
 }
 
+function renderTimeBlocks() {
+    let row = $("<div>").addClass("row input-group mb-3");
+        let timeBlock = $("<div>").addClass("input-group-prepend time-block");
+            let hour = moment(9, "h").format("ha");
+            let span = $("<span>").addClass("input-group-text hour").text(hour);
+            timeBlock.append(span);
+        let textArea = $("<input>").attr("type", "text").addClass("form-control textarea")
+            .attr("placeholder", "Enter task here").attr("aria-label", "Enter task here")
+            .attr("aria-describedby", "button-addon2");
+        let buttonDiv = $("<div>").addClass("input-group-append");
+            let saveButton = $("<button>").addClass("btn btn-outline-secondary saveBtn")
+                .attr("type", "button").attr("id", "button-addon2").text("Save");
+            buttonDiv.append(saveButton);
+        row.append(timeBlock, textArea, buttonDiv);
+    container.append(row);
+}
+
+
+renderTimeBlocks()
+
+
+
+
+
+
+
+
+
+
+
+
+
 setInterval(displayDay, 1000);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 });
